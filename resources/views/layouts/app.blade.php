d43f06d12da48ce67d3dbf9b40b3a415b09cae5e
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-10-22 23:16:06 +0200

    Aggiunte img

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>@yield('page-title') | Laravel Start 1</title>
+        <title>@yield('page-title') | Laravel Comics</title>
 
         {{-- Stili nell'head --}}
         <link rel="stylesheet" href="css/style.css">
@@ -15,34 +15,18 @@
         @yield('head-imports')
     </head>
     <body>
-
         @include('partials.header')
+        <div>
 
-        <main>
-            <div class="container">
-
-                <div class="row">
-
-                    <div class="col">
-
-                        <div>
-                            <img class="gatto-img" src="{{ Vite::asset('resources/img/gatto.webp') }}" alt="Gatto">
-                        </div>
-
-                        <div class="gatto-bg">
-                            QUESTO DIV AVRA' COME SFONDO IL GATTO
-                        </div>
-
-                        @yield('main-content')
-
-                    </div>
-
-                </div>
-
-            </div>
-        </main>
-
+        </div>
+        @yield('main-content')
+        <div>
+         Main
+        </div>
         @include('partials.footer')
+        <div>
+
+        </div>
 
         {{-- JS da importare nel body --}}
         @yield('body-imports')
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,39 +1,15 @@
-@php
-    $links = [
-        [
-            'url' => '/',
-            'label' => 'Home',
-            'active' => true,
-        ],
-        [
-            'url' => '/chi-siamo',
-            'label' => 'Chi siamo',
-            'active' => true,
-        ],
-        [
-            'url' => '/contatti',
-            'label' => 'Contatti',
-            'active' => false,
-        ],
-    ];
-@endphp
-
 <header>
     <nav>
+        <figure>
+            <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="Logo" height="60">
+        </figure>
         <ul>
-            @foreach ($links as $link)
-                <li>
-                    @if ($link['active'])
-                        <a href="{{ $link['url'] }}">
-                            {{ $link['label'] }}
-                        </a>
-                    @else
-                        <del>
-                            {{ $link['label'] }}
-                        </del>
-                    @endif
-                </li>
+            @foreach ($menuLinks as $element)
+            <li>
+                <a href="#">{{$element['name'] }}</a>
+            </li>
             @endforeach
         </ul>
     </nav>
+    
 </header>
